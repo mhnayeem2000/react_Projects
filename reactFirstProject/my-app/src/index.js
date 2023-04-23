29f98d73f3565991462048932d8ebd5e9e28efdf
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-24 02:24:01 +0600

    ract 1st app

---
+++ reactFirstProject/my-app/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <div className='maindiv'>
+      <h1 className=' bigTitle'>ToDo Application</h1>
+      <div className='card'>
+         <h2 className='cardTitle'>Call Family</h2>
+         <p className='cardPara'> vai import css jodi likha thake javascript e but ami  public folder e link kore css kortlam but out put dekhailo na...karon ki
+         vai import css jodi likha thake javascript e but ami   folder e link kore css kortlam but out put dekhailo na...karon ki
+         </p>
+         <button className='button'>Knock Me</button>
+         <p className='cardDate'>15/02/2023</p>
+      </div>
+   </div>  
+
+
+   
+);
+
+

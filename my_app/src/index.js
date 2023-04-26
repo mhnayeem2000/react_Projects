e740bf4dc499347ed4d5f0d65605e9d6746fee07
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-04-26 13:09:20 +0600

    add external css

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -10,16 +10,16 @@ const day = date.getDate();
 const month = date.getMonth()+1;
 const year = date.getFullYear();
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <div style={{width:"300px",height:"300px" , background:"pink",textAlign:"center",padding:"5px"}} >
-        
-        <h1 style={{color :"red" , fontSize:"25px" , fontFamily:"sans-serif" , paddingTop:"20px"}} >{name}</h1>
-        <h6>Student of CSE</h6>
+      <div className='boxStyle' >
+        <h1 className='h1Style' >{name}</h1>
+        <h4 className='h4Style'  >-Student of CSE</h4>
         <p>lorem ipsum doman do itsa kasur manus is lareo kilo isat manush kintu na</p>
-        <p>{day + "/" + month + "/" + year}</p>
+        <p><b>{day + "/" + month + "/" + year}</b></p>
       </div>
     </div>
   </React.StrictMode>
